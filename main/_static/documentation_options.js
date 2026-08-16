cdc97de469353246e6696243ab04908c9a640872
gilesknap <gilesknap@users.noreply.github.com>
2026-08-16 15:12:34 +0000

    deploy: a7af2e17e22a0db2e6ad71d9fc8a9f48f780e82b

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0b5.dev13+g48b64eb4e',
+    VERSION: '0.1.0b5.dev26+ga7af2e17e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
